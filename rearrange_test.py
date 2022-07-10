0680cfb43ba53d44286cddff2f61e5db95831018
amsem <aminesaidsel@gmail.com>
2022-07-10 20:21:49 +0100

    First Unit test

--- rearrange_test.py
+++ rearrange_test.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 
+from rearrange import rearrange_name
+import unittest
+
+class TestRearrage(unittest.TestCase):
+	def test_basic(self):
+		testcase = "Musk, Elon"
+		expected = "Elon Musk"
+		self.assertEqual(rearrange_name(testcase), expected)
+
+unittest.main() 
+
 
